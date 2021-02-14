2b96fd62d74e27678a06c2502804504af3963a3b
Muhammad Sadewa Wicaksana W <wicaksanasadewa@yahoo.com>
2021-02-14 12:15:51 +0700

    --> Init

---
+++ app/src/main/java/com/codedirect/appiumtestapk/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.codedirect.appiumtestapk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
